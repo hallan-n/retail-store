1ea169354fe0593a43b4cc6398db890b08429891
hallan-n <hallanneves01@gmail.com>
2024-02-06 16:54:19 -0300

    feat: created header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgOptimizedImage } from '@angular/common';
-import { RouterLinkActive } from '@angular/router';
-import { ButtonComponent } from '../button/button.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
 
+import {MatListModule} from '@angular/material/list';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage, RouterLinkActive, ButtonComponent],
+  imports: [MatIconModule, MatMenuModule, MatButtonModule, MatToolbarModule, NgOptimizedImage, MatSelectModule, MatInputModule, MatFormFieldModule, MatDividerModule, MatGridListModule, MatListModule],
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent { }
