fbaa1cdf17d8386ec0e546c7d41cc09362cf342e
hallan-n <hallanneves01@gmail.com>
2024-02-07 09:12:27 -0300

    refactor: add card responsive

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { NgOptimizedImage } from '@angular/common';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, MatCardModule],
   templateUrl: './card.component.html'
 })
 export class CardComponent {
